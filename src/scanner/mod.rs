23d687b5b9f2b7787b72c609ac9b500c2be0d71f
H12H <sucodetr@protonmail.com>
2024-05-28 16:54:55 +0530

    removed the redundatant Default trait

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -2,20 +2,17 @@
 
 //TODO: Expression should be a link list of the following types but try brainstorm it but there will be problems
 
-use std::default;
+type Expression = Vec<Pattern>;
 
-type Expression = Vec<Pattern> ;
-
-#[derive(Debug, Default,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 struct Pattern {
     sub_pattern: SubPattern,
     repetition: Option<Repetition>,
 }
 
 // SubPattern together form Pattern
-#[derive(Debug, Default, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum SubPattern {
-    #[default]
     Dot,
     Char(char),
     InvertedChar(char),
@@ -23,9 +20,8 @@ enum SubPattern {
     InvertedSet(Sets),
 }
 
-#[derive(Debug, Default,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum Sets {
-    #[default]
     AlphaNum,
     Alpha,
     Blank, // TODO: how is that different from the space
@@ -43,7 +39,7 @@ enum Sets {
 }
 
 // The custom range will be like this [0-9A-Za-z]
-#[derive(Debug, Default,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 struct Range {
     // both start and end are inclusive
     start: char,
@@ -56,34 +52,33 @@ enum Anchor {
     End,
 }
 
-#[derive(Debug, Default,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum Repetition {
-    AtMostOnce,  // ?
-    AtLeastOnce, // +
-    #[default]
-    ZeroOrMore, // *     //TODO: suggest a better name
-    Exactly(usize), // {n}
-    AtLeast(usize), // {n,}
-    AtMost(usize), // {,m}
+    AtMostOnce,            // ?
+    AtLeastOnce,           // +
+    ZeroOrMore,            // *     //TODO: suggest a better name
+    Exactly(usize),        // {n}
+    AtLeast(usize),        // {n,}
+    AtMost(usize),         // {,m}
     Between(usize, usize), // {n,m} // TODO: suggest a better name
 }
 
 #[test]
 fn t1() {
+    use std::iter::Peekable;
     use std::str::Chars;
-    use  std::iter::Peekable;
 
     let exp = "s.+e";
     let ans = vec![
-        Pattern{
+        Pattern {
             sub_pattern: SubPattern::Char('s'),
             repetition: None,
         },
-        Pattern{
+        Pattern {
             sub_pattern: SubPattern::Dot,
             repetition: Some(Repetition::AtLeastOnce),
         },
-        Pattern{
+        Pattern {
             sub_pattern: SubPattern::Char('e'),
             repetition: None,
         },
@@ -93,39 +88,35 @@ fn t1() {
         // cannot trim the line
         let mut iter = line.chars().peekable();
         let check_repetition = |iter: &mut Peekable<Chars<'a>>| match iter.peek() {
-           Some('+') => {
+            Some('+') => {
                 let _ = iter.next();
                 Some(Repetition::AtLeastOnce)
             }
-           Some('?') => {
+            Some('?') => {
                 let _ = iter.next();
                 Some(Repetition::AtMostOnce)
             }
-           Some('*') => {
+            Some('*') => {
                 let _ = iter.next();
                 Some(Repetition::ZeroOrMore)
             }
-           Some('{') => unreachable!("did not implement that"),
+            Some('{') => unreachable!("did not implement that"),
             _ => None,
         };
 
         let mut expression: Expression = Vec::new();
         while let Some(ch) = iter.next() {
             let pattern = match ch {
-                '.' => 
-                    Pattern {
-                      sub_pattern: SubPattern::Dot,
-                      repetition: check_repetition(&mut iter),
-                    }
-                ,            
+                '.' => Pattern {
+                    sub_pattern: SubPattern::Dot,
+                    repetition: check_repetition(&mut iter),
+                },
                 // need to include other ascii char too later
-                'a'..='z' | '0'..='9' | 'A'..='Z' => 
-                    Pattern {
-                      sub_pattern: SubPattern::Char(ch),
-                      repetition: check_repetition(&mut iter),
-                    }
-                ,
-                _ => unreachable!("Lets see what is that: {:#?}", (ch, iter))
+                'a'..='z' | '0'..='9' | 'A'..='Z' => Pattern {
+                    sub_pattern: SubPattern::Char(ch),
+                    repetition: check_repetition(&mut iter),
+                },
+                _ => unreachable!("Lets see what is that: {:#?}", (ch, iter)),
             };
             expression.push(pattern);
         }
